47237d38301fd7ef51bd2c7e2d1744fc2be7bb75
leeya018 <leeyahav018@gmail.com>
2024-02-04 14:22:57 +0200

    accrodion

--- src/components/questionList/index.tsx
+++ src/components/questionList/index.tsx
@@ -1,5 +1,11 @@
 import React, { useEffect, useState } from "react"
 import { observer } from "mobx-react-lite"
+import Accordion from "@mui/material/Accordion"
+import AccordionSummary from "@mui/material/AccordionSummary"
+import AccordionDetails from "@mui/material/AccordionDetails"
+import Typography from "@mui/material/Typography"
+import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward"
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown"
 
 type QuestionListItem = {
   question: string
@@ -12,7 +18,22 @@ const QuestionList = observer<QuestionListProps>(({ questionList }) => {
   return (
     <ul>
       {questionList.map((questionListItem: QuestionListItem, key: number) => (
-        <li key={key}>{questionListItem.question}</li>
+        <li key={key}>
+          <Accordion>
+            <AccordionSummary
+              expandIcon={<ArrowDownwardIcon />}
+              aria-controls="panel1-content"
+              id="panel1-header"
+            >
+              <Typography className="border-b-2">
+                {questionListItem.question}
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>{questionListItem.answer}</Typography>
+            </AccordionDetails>
+          </Accordion>
+        </li>
       ))}
     </ul>
   )
